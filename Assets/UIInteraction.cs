c64d2aca1458b2a2b07607d984f719ba2dcdbaeb
AgitAngst <markiz.de.sad@gmail.com>
2020-12-05 14:46:09 +0300

    update 1
    Added ui to the game
    Added Ui call on E press

---
+++ Assets/UIInteraction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.UI;
+
+public class UIInteraction : MonoBehaviour
+{
+    public UIView playerView;
+    bool isViewing = false;
+    public SFPSC_FPSCamera playerCamera;
+    SFPSC_PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<SFPSC_PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isViewing)
+            {
+                isViewing = true;
+                PlayerActions(!isViewing);
+                playerView.Show();
+            }
+            else
+            {
+                isViewing = false;
+                PlayerActions(!isViewing);
+
+                playerView.Hide();
+
+            }
+        }
+         
+    }
+
+    public void PlayerActions(bool isEnabled)
+    {
+        if (isEnabled)
+        {
+            player.enabled = true;
+            playerCamera.enabled = true;
+        }
+        else
+        {
+            player.enabled = false;
+            playerCamera.enabled = false;
+        }
+    }
+
+}
